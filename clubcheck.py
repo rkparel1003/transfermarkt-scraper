fe146af34f021f25a96cda0c7fe4dd99dd3088d8
rkparel1003 <rkparel1003@gmail.com>
2019-08-17 00:40:43 -0700

    Added scraping for position 	+Moved scraping into their own functions for modularity

--- clubcheck.py
+++ clubcheck.py
@@ -2,10 +2,29 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import pandas as pd
 
+
+def _find_position(player):
+    return player.find("td")["title"]
+
+
 def _find_nationality(player):
     for tag in player.findAll("img"):
         if len(tag["class"]) > 0 and tag["class"][0] == "flaggenrahmen":
             return tag["alt"]
+
+
+def _find_player_name(player):
+    player_name = player.find("img")["title"]
+    if "Joined" in player_name or "Returned" in player_name:
+        player_name = player.find("a", {"class": "spielprofil_tooltip tooltipstered"}).getText()
+    return player_name
+
+def _scrape_table(table, row_type):
+    for player in table.findAll("tr", {"class":row_type}):
+        player_name = _find_player_name(player)
+        player_nationality = _find_nationality(player)
+        player_position = _find_position(player)
+        print(player_name, player_nationality, player_position)
 
 
 def scrape_club(url:str):
@@ -14,18 +33,8 @@ def scrape_club(url:str):
     content = driver.page_source
     soup = BeautifulSoup(content, features="html.parser")
     table = soup.find("table", {"class":"items"})
-    for player in table.findAll("tr", {"class":"odd"}):
-        player_name = player.find("img")["title"]
-        if "Joined" in player_name or "Returned" in player_name:
-            player_name = player.find("a",{"class":"spielprofil_tooltip tooltipstered"}).getText()
-        player_nationality = _find_nationality(player)
-        print(player_name, player_nationality)
-    for player in table.findAll("tr", {"class":"even"}):
-        player_name = player.find("img")["title"]
-        if "Joined" in player_name or "Returned" in player_name:
-            player_name = player.find("a", {"class": "spielprofil_tooltip tooltipstered"}).getText()
-        player_nationality = _find_nationality(player)
-        print(player_name, player_nationality)
+    _scrape_table(table, "odd")
+    _scrape_table(table, "even")
 
 
 if __name__ == "__main__":
