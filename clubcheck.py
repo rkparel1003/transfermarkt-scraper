1a073059a3e73ef3e2e2d54dc0fdcbaa9877cb31
rkparel1003 <rkparel1003@gmail.com>
2019-08-21 00:56:48 -0700

    Added scraping for the entire row of the table on the club page.

--- clubcheck.py
+++ clubcheck.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import pandas as pd
 
-
 def _find_position(player):
     return player.find("td")["title"]
 
@@ -13,19 +12,39 @@ def _find_nationality(player):
             return tag["alt"]
 
 
-def _find_player_name(player):
+def _find_player_name(player, player_dict):
     player_name = player.find("img")["title"]
     if "Joined" in player_name or "Returned" in player_name:
         player_name = player.find("a", {"class": "spielprofil_tooltip tooltipstered"}).getText()
     return player_name
 
-def _scrape_table(table, row_type):
-    for player in table.findAll("tr", {"class":row_type}):
-        player_name = _find_player_name(player)
-        player_nationality = _find_nationality(player)
-        player_position = _find_position(player)
-        print(player_name, player_nationality, player_position)
 
+def _scrape_table(table, row_type):
+    player_dict = dict()
+    for player in table.findAll("tr", {"class":row_type}):
+        player_name = _find_player_name(player, player_dict)
+        player_nationality = _find_nationality(player, player_dict)
+        player_position = _find_position(player, player_dict)
+        #number, birthday, BLANK, height, footed, join date, BLANK, end contract
+        next = player.find_next("td",{"class":"zentriert"})
+        player_number = next.getText()
+        next = next.find_next("td", {"class": "zentriert"})
+        player_birthday = next.getText()
+        # this one is a blank
+        next = next.find_next("td", {"class": "zentriert"})
+        next = next.find_next("td", {"class": "zentriert"})
+        player_height = next.getText()
+        next = next.find_next("td", {"class": "zentriert"})
+        player_foot = next.getText()
+        next = next.find_next("td", {"class":"zentriert"})
+        player_join_date = next.getText()
+        # blank
+        next = next.find_next("td", {"class":"zentriert"})
+        next = next.find_next("td", {"class": "zentriert"})
+        player_contract_until = next.getText()
+        next = next.find_next("td", {"class": "rechts hauptlink"})
+        player_current_value = next.getText()
+        print(player_name, player_nationality, player_position,player_number, player_birthday, player_height, player_foot, player_join_date, player_contract_until, player_current_value)
 
 def scrape_club(url:str):
     driver = webdriver.Firefox()
