62b910504e3abb5f39ca7394ce55967f0f19a2b5
rkparel1003 <rkparel1003@gmail.com>
2019-08-24 09:35:45 -0700

    Constants file

---
+++ transfermarkt-scraper/ScraperConstants.py
@@ -0,0 +1,11 @@
+"""
+CONSTANTS
+"""
+
+HEADER = "https://www.transfermarkt.us"
+
+GB1_URL = "https://www.transfermarkt.us/premier-league/startseite/wettbewerb/GB1"
+ES1_URL = "https://www.transfermarkt.us/la-liga/startseite/wettbewerb/ES1"
+IT1_URL = "https://www.transfermarkt.us/serie-a/startseite/wettbewerb/IT1"
+L1_URL = "https://www.transfermarkt.us/1-bundesliga/startseite/wettbewerb/L1"
+FR1_URL = "https://www.transfermarkt.us/ligue-1/startseite/wettbewerb/FR1"
