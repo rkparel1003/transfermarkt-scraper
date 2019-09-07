597474123bbd3d885441cf858a5093ac44198ca5
rkparel1003 <rkparel1003@gmail.com>
2019-09-07 16:20:55 -0700

    Unit test for scraping an entire league and inserting its data into the db

--- tests/test_insertCompetitionIntoDatabase.py
+++ tests/test_insertCompetitionIntoDatabase.py
@@ -1,9 +1,19 @@
 import unittest
 
+from CompetitionScraper import CompetitionScraper
+from ClubScraper import ClubScraper
+import ScraperConstants
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+def generic_test_scrape_and_insert(competition_url):
+    scraper = CompetitionScraper(competition_url)
+    scraper.scrape_competition()
+    scraper.insert_all_clubs_data()
+    scraper.print_database()
+
+
+class TestInsertCompetitionIntoDatabase(unittest.TestCase):
+    def test_IT1(self):
+        generic_test_scrape_and_insert(ScraperConstants.IT1_URL)
 
 
 if __name__ == '__main__':
