17ee4650de76b1a1af5f5c7ae32e5fd0da13150f
rkparel1003 <rkparel1003@gmail.com>
2019-09-10 00:17:03 -0700

    Type annotations and Comments added to functions in ClubScraper and CompetitionScraper

--- transfermarkt-scraper/ClubScraper.py
+++ transfermarkt-scraper/ClubScraper.py
@@ -1,9 +1,10 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
-import ScraperConstants
+from typing import MutableMapping
+
 
 class ClubScraper:
-    def __init__(self, name, url):
+    def __init__(self, name: str, url: str):
         self._url = url
         self._table = None
         self._club_name = name
@@ -11,58 +12,92 @@ class ClubScraper:
         self._current_player_number = int()
         self.driver = webdriver.Firefox()
 
-    def _find_player_number(self, tag):
+    '''
+        Extracts the player's number from the provided tag.
+    '''
+    def _find_player_number(self, tag: BeautifulSoup) -> BeautifulSoup:
         number_tag = tag.find_next("td", {"class": "zentriert"})
         self._current_player_number = number_tag.getText()
         self._player_dict[self._current_player_number] = dict()
         self._player_dict[self._current_player_number]["number"] = number_tag.getText()
         return number_tag
 
-    def _find_position(self, player):
+    '''
+        Extracts the player's position from the provided tag.
+    '''
+    def _find_position(self, player: BeautifulSoup) -> None:
         self._player_dict[self._current_player_number]["position"] = player.find("td")["title"]
 
-    def _find_nationality(self, player):
+    '''
+        Extracts the player's nationality from the provided tag.
+    '''
+    def _find_nationality(self, player: BeautifulSoup) -> None:
         for tag in player.findAll("img"):
             if len(tag["class"]) > 0 and tag["class"][0] == "flaggenrahmen":
                 self._player_dict[self._current_player_number]["nationality"] = tag["alt"]
 
-    def _find_player_name(self, player):
+    '''
+        Extracts the player's name from the provided tag.
+    '''
+    def _find_player_name(self, player: BeautifulSoup) -> None:
         player_name = player.find("img")["title"]
         if "Joined" in player_name or "Returned" in player_name:
             player_name = player.find("a", {"class": "spielprofil_tooltip tooltipstered"}).getText()
         self._player_dict[self._current_player_number]["name"] = player_name
 
-    def _find_player_birthday(self, tag):
+    '''
+        Extracts the player's birthday from the provided tag.
+    '''
+    def _find_player_birthday(self, tag: BeautifulSoup) -> BeautifulSoup:
         birthday_tag = tag.find_next("td", {"class": "zentriert"})
         self._player_dict[self._current_player_number]["birthday"] = birthday_tag.getText()
         return birthday_tag
 
-    def _find_player_height(self, tag):
+    '''
+        Extracts the player's height from the provided tag.
+    '''
+    def _find_player_height(self, tag: BeautifulSoup) -> BeautifulSoup:
         height_tag = tag.find_next("td", {"class": "zentriert"})
         self._player_dict[self._current_player_number]["height"] = height_tag.getText()
         return height_tag
 
-    def _find_player_foot(self, tag):
+    '''
+        Extracts the player's foot preference from the provided tag.
+    '''
+    def _find_player_foot(self, tag: BeautifulSoup) -> BeautifulSoup:
         foot_tag = tag.find_next("td", {"class": "zentriert"})
         self._player_dict[self._current_player_number]["foot"] = foot_tag.getText()
         return foot_tag
 
-    def _find_player_join_date(self, tag):
+    '''
+        Extracts the player's join_date from the provided tag.
+    '''
+    def _find_player_join_date(self, tag: BeautifulSoup) -> BeautifulSoup:
         join_date_tag = tag.find_next("td", {"class": "zentriert"})
         self._player_dict[self._current_player_number]["join_date"] = join_date_tag.getText()
         return join_date_tag
 
-    def _find_player_contract(self, tag):
+    '''
+        Extracts the player's contract expiration date from the provided tag.
+    '''
+    def _find_player_contract(self, tag: BeautifulSoup) -> BeautifulSoup:
         contract_tag = tag.find_next("td", {"class": "zentriert"})
         self._player_dict[self._current_player_number]["contract"] = contract_tag.getText()
         return contract_tag
 
-    def _find_player_value(self, tag):
+    '''
+        Extracts the player's value from the provided tag.
+    '''
+    def _find_player_value(self, tag: BeautifulSoup) -> BeautifulSoup:
         value_tag = tag.find_next("td", {"class": "rechts hauptlink"})
         self._player_dict[self._current_player_number]["value"] = value_tag.getText(strip=True)
         return value_tag
 
-    def _scrape_row(self, player):
+    '''
+        Each player is one row in the table.
+        Sequentially extracts each piece of data from this tag.
+    '''
+    def _scrape_row(self, player: BeautifulSoup) -> None:
         number_tag = self._find_player_number(player)
         self._find_player_name(player)
         self._find_nationality(player)
@@ -78,11 +113,18 @@ class ClubScraper:
         contract_tag = self._find_player_contract(blank_tag)
         value_tag = self._find_player_value(contract_tag)
 
-    def _scrape_table(self, row_type):
+    '''
+        Scrapes every row in the table that matches the odd or even row type provided.
+    '''
+    def _scrape_table(self, row_type: str) -> None:
         for player in self._table.findAll("tr", {"class": row_type}):
             self._scrape_row(player)
 
-    def scrape_club(self):
+    '''
+        Extracts the table of player data from the url provided in the constructor.
+        Scrapes over each row of the table and returns a dictionary of all players in this club.
+    '''
+    def scrape_club(self) -> MutableMapping[str, MutableMapping[str, str]]:
         self.driver.get(self._url)
         content = self.driver.page_source
         soup = BeautifulSoup(content, features="html.parser")
@@ -93,4 +135,3 @@ class ClubScraper:
 
     def __del__(self):
         self.driver.quit()
-
--- transfermarkt-scraper/CompetitionScraper.py
+++ transfermarkt-scraper/CompetitionScraper.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
+from typing import MutableMapping, Optional, Dict
 
 from ClubScraper import ClubScraper
 import ScraperConstants
@@ -7,15 +8,19 @@ import transfermarktdatabase
 
 
 class CompetitionScraper:
-    def __init__(self, competition_url):
+    def __init__(self, competition_url: str):
         self._url = competition_url
         self._table = None
-        self.teams = dict()
+        self.teams = Dict[str, str]
         self.driver = webdriver.Firefox()
         self.dbms = transfermarktdatabase.MyDatabase(transfermarktdatabase.SQLITE, dbname="transfermarktdb.sqlite")
         self.dbms.create_db_tables()
 
-    def _scrape_table(self, row_type):
+    '''
+        Pulls data from the rows of the table.
+        Reads from all odd rows and then all even rows in the order provided.
+    '''
+    def _scrape_table(self, row_type: str) -> None:
         for player in self._table.findAll("tr", {"class": row_type}):
             club_tag = player.find_next("a", {"class":"vereinprofil_tooltip tooltipstered"})
             club_name = club_tag.contents[0]["alt"]
@@ -23,7 +28,12 @@ class CompetitionScraper:
             club_link = club_link.replace("startseite", "kader")
             self.teams[club_name] = club_link
 
-    def scrape_competition(self):
+    '''
+        Uses the selenium webdriver to scrape the competition url provided in 
+        CompetitionScraper's constructor.
+        Looks over all of the rows of players on this page.
+    '''
+    def scrape_competition(self) -> None:
         self.driver.get(self._url)
         content = self.driver.page_source
         soup = BeautifulSoup(content, features="html.parser")
@@ -31,16 +41,26 @@ class CompetitionScraper:
         self._scrape_table("odd")
         self._scrape_table("even")
 
-    def insert_all_clubs_data(self):
+    '''
+        Scrapes all of the club urls in the teams dictionary.
+    '''
+    def insert_all_clubs_data(self) -> None:
         for team_name in self.teams:
             self._scrape_club(team_name)
 
-    def _scrape_club(self, club_name):
+    '''
+        Creates a club scraper and scrapes the url that the club_name parameter
+        maps to in self.teams.
+    '''
+    def _scrape_club(self, club_name: str) -> None:
         club_scraper = ClubScraper(club_name, self.teams[club_name])
         players = club_scraper.scrape_club()
         self._insert_club_data(club_name, players)
 
-    def print_clubs(self):
+    '''
+        Prints out all key value pairs of (name, club_info dictionary).
+    '''
+    def print_clubs(self) -> None:
         for key in self.teams:
             print(key, self.teams[key])
 
@@ -49,7 +69,7 @@ class CompetitionScraper:
         Calls for an insert query to be created with this players' data
         Tells the dbms to execute the query.
     '''
-    def _insert_club_data(self, club_name, players):
+    def _insert_club_data(self, club_name: str, players: MutableMapping[str, MutableMapping[str, str]]):
         for key in players:
             player = players[key]
             insert_query_values = self._create_insert_query(club_name, player)
@@ -60,7 +80,7 @@ class CompetitionScraper:
         Pulls out every variable from the player dictionary and puts it into a string 
         formatted as a SQL Insert statement.
     '''
-    def _create_insert_query(self, club_name, player: dict):
+    def _create_insert_query(self, club_name, player: MutableMapping[str, str]) -> tuple:
         player_id = self._get_player_id()
         number = player['number']
         name = player['name']
@@ -73,22 +93,35 @@ class CompetitionScraper:
         price = player['value']
         return player_id, club_name, number, name, nationality, position, birthday, height, join_date, contract, price
 
-    def single_insert(self, query, values=None):
-        self.dbms.execute_query(values, query)
-
-    def _get_player_id(self):
+    '''
+        Gets the unique ID for the player id.
+    '''
+    def _get_player_id() -> int:
         player_id = ScraperConstants.max_id
         ScraperConstants.max_id += 1
         return player_id
 
-    def print_database(self):
+    '''
+        Performs a single insert query onto the player database.
+        values may or may not be provided.
+    '''
+    def single_insert(self, query, values: Optional[tuple] = None):
+        self.dbms.execute_query(values, query)
+
+    '''
+        Calls the function in transfermarktdatabase to print out all player rows.
+    '''
+    def print_database(self) -> None:
         self.dbms.print_all_data(transfermarktdatabase.PLAYERS)
 
-    def __getitem__(self, item):
+    def __getitem__(self, item: str) -> str:
         return self.teams[item]
 
-    def __setitem__(self, key, value):
-        pass
+    def __setitem__(self, key: str, value: str) -> None:
+        self.teams[key] = value
 
-    def __del__(self):
+    '''
+        When this class leaves scope it closes the selenium webdriver.
+    '''
+    def __del__(self) -> None:
         self.driver.quit()
