79b3b1b44386cf00b92a605c2404b4bf357eb71b
rkparel1003 <rkparel1003@gmail.com>
2019-12-22 14:36:09 -0800

    Add USER_AGENT string to Constants file

--- scraper/ScraperConstants.py
+++ scraper/ScraperConstants.py
@@ -4,6 +4,9 @@ from typing import Final
 CONSTANTS
 """
 
+USER_AGENT :Final[str] = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'
+HEADS = {'User-Agent': USER_AGENT}
+
 HEADER: Final[str] = "https://www.transfermarkt.us"
 GB1_URL: Final[str] = "https://www.transfermarkt.us/premier-league/startseite/wettbewerb/GB1"
 ES1_URL: Final[str] = "https://www.transfermarkt.us/la-liga/startseite/wettbewerb/ES1"
